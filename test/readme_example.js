c47b1d9ea3ce42095ad39f3c74efa48eaffb0814
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-02-29 13:39:03 +0000

    Add key in example

--- test/readme_example.js
+++ test/readme_example.js
@@ -3,5 +3,9 @@ const forkeys = require('../index.js');
 (() => {
     "use strict";
 
-    
+    let obj = {
+        firstKey: 0,
+        secondKey: "bla",
+        final_key: "last one"
+    };
 })();
