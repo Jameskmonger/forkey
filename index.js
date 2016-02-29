75a25b755fe1848335b8d764978e5c81ca88e030
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-02-29 13:37:34 +0000

    Remove unnecessary brackets

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 ((m) => {
     "use strict";
 
-    m.exports = (obj, cb) => {
-        Object.keys(obj).forEach(cb);
-    };
+    m.exports = (obj, cb) => Object.keys(obj).forEach(cb);
 })(module);
